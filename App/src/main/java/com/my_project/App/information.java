dddda3c6e1b3fb95f37301abaaeb974bd89b8077
Swanand <swanandj.work@gmail.com>
2026-08-07 18:31:06 +0530

    Added more student information

--- App/src/main/java/com/my_project/App/information.java
+++ App/src/main/java/com/my_project/App/information.java
@@ -8,12 +8,17 @@ import java.util.Map;
 import java.util.HashMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/student")
 public class information {
     private Map<String,student>map = new HashMap<>();
+    private Map<Long, student>map_id = new HashMap<>();
+    private Map<Long, student>map_age = new HashMap<>();
+    private Map<String, student>map_course = new HashMap<>();
     @GetMapping("/hello")
     public String sayHello() {
         return new String("Hello, there!");
@@ -22,10 +27,25 @@ public class information {
     public boolean createStudent(@RequestBody student s) {
         //TODO: process POST request
         map.put(s.getName(),s);
+        map_id.put(s.getId(),s);
+        map_age.put(s.getAge(), s);
+        map_course.put(s.getCourse(), s);
         return true;
     }
-    @GetMapping("/get/{name}")
-    public student studentID(@PathVariable String name) {
+    @GetMapping("/get/id/{id}")
+    public student studentId(@PathVariable Long id) {
+        return map_id.get(id);
+    }
+    @GetMapping("/get/name/{name}")
+    public student studentName(@PathVariable String name) {
         return map.get(name);
+    }
+    @GetMapping("/get/age/{age}")
+    public student studentAge(@PathVariable Long age) {
+        return map_age.get(age);
+    }
+    @GetMapping("/get/course/{course}")
+    public student studentCourse(@PathVariable String course) {
+        return map_course.get(course);
     }
 }
