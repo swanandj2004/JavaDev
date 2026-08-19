c781d00e000184353fe7620fd7cf7670b6f0c902
Swanand <swanandj.work@gmail.com>
2026-08-19 12:37:21 +0530

    Created User Repository and Security Configuration

---
+++ Auth/src/main/java/com/example/Auth/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.example.Auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- Auth/src/main/java/com/example/Auth/User.java
+++ Auth/src/main/java/com/example/Auth/User.java
@@ -5,13 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -23,13 +18,29 @@ public class User {
     private String username;
     @Column(nullable = false)
     private String password;
-    private boolean enabled=true;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private int failedAttempts;
+    private boolean accountLocked;
 
-    @ManyToMany
-    @JoinTable(
-        name = "user_roles",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role>userRoles = new HashSet<>();
+    public User() { }
+
+    public void setUsername(String username) {
+        this.username=username;
+    }
+    public void setEmail(String email) {
+        this.email=email;
+    }
+    public void setPassword(String password) {
+        this.password=password;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
 }
